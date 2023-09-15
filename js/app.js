cbcb8efe143530f7484f2c9bf0952bc9fcc546bc
kaiden-ong <kaiden.ong000@gmail.com>
2023-09-15 14:13:21 -0700

    Footer email function complete (at least functional)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,58 @@
+// form functions
 function openForm() {
+  document.querySelector(".form-bg").style.display = "block";
   document.getElementById("myForm").style.display = "block";
+  disableScroll();
 }
 
 function closeForm() {
+  document.querySelector(".form-bg").style.display = "none";
   document.getElementById("myForm").style.display = "none";
   resetForm();
+  enableScroll();
 }
 
 function resetForm() {
   document.querySelector(".form-container").reset();
+}
+
+// disable scrolling
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
