7ee89ff3b476a39ead5d285270301f3862d26f50
kaiden-ong <kaiden.ong000@gmail.com>
2023-09-19 20:11:35 -0700

    ADding to pupsupport project page

--- js/app.js
+++ js/app.js
@@ -57,8 +57,6 @@ const observer = new IntersectionObserver((entries) => {
     console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
     }
   });
 });
