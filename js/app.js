272647537f01e900b7e47c2882034bc49abd8949
kaiden-ong <kaiden.ong000@gmail.com>
2023-09-21 21:01:33 -0700

    Removed console logs and copied pupsupport to other project pages

--- js/app.js
+++ js/app.js
@@ -54,7 +54,6 @@ function topFunction() {
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
     }
